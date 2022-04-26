5d6edcd18e2e91b4d449f2a616afc7d41e9e5286
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-25 19:20:40 -0700

    "Functionalized" Form component

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
-
 import './form.scss';
 
+function Form({ handleApiCall }) {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      method: 'GET',
+      url: 'https://pokeapi.co/api/v2/pokemon',
+    };
+    handleApiCall(formData);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label >
+          <span>URL: </span>
+          <input name='url' type='text' />
+          <button type="submit">GO!</button>
+        </label>
+        <label className="methods">
+          <span id="get">GET</span>
+          <span id="post">POST</span>
+          <span id="put">PUT</span>
+          <span id="delete">DELETE</span>
+        </label>
+      </form>
+    </>
+  );
+}
+
+export default Form;
+
+/*
 class Form extends React.Component {
 
   handleSubmit = e => {
@@ -33,5 +64,4 @@ class Form extends React.Component {
     );
   }
 }
-
-export default Form;
+*/
