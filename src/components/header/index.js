28bc12692c4a957475db18f5e0b16371661a3bd0
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-25 19:20:10 -0700

    Removed commented-out code

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,15 +7,4 @@ function Header() {
   );
 }
 
-/*
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <h1>RESTy</h1>
-      </header>
-    );
-  }
-}
-*/
 export default Header;
--- src/components/results/index.js
+++ src/components/results/index.js
@@ -7,15 +7,4 @@ function Results ({ data }) {
   );
 }
 
-/*
-class Results extends React.Component {
-  render() {
-    return (
-      <section>
-        <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
-      </section>
-    );
-  }
-}
-*/
 export default Results;
