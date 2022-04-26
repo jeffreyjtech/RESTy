2ffc1e47b7a3072e85efc1408d5a424c7ef3a4bc
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-25 21:27:32 -0700

    import results scss

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,7 +1,9 @@
+import './results.scss'
 
 function Results ({ data }) {
   return(
     <section>
+      <span><em>Results</em></span>
       <pre>{data ? JSON.stringify(data, undefined, 2) : null}</pre>
     </section>
   );
