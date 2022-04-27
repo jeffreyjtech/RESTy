be20afd9841ac4189abc38f240b515205310cd2f
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-26 21:49:13 -0700

    commiting changes before deleting local repo

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,11 +1,17 @@
 import './results.scss'
 
-function Results ({ data }) {
-  return(
-    <section>
-      <span><em>Results</em></span>
-      <pre>{data ? JSON.stringify(data, undefined, 2) : null}</pre>
-    </section>
+function Results({ data }) {
+  return (
+    <>
+      {data ?
+        <section>
+          <span><em>Results</em></span>
+          <pre>{JSON.stringify(data, undefined, 2)}</pre>
+        </section> :
+        <section>
+          <span><em>Waiting on response. . .</em></span>
+        </section>}
+    </>
   );
 }
 
