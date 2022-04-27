77ea39835935940872d476e24b2fefafa8ed2475
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-26 23:10:05 -0700

    Refactored App as a functional component with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 import './app.scss';
 
@@ -9,6 +9,39 @@ import Footer from './components/footer';
 import Form from './components/form';
 import Results from './components/results';
 
+function App() {
+
+  let [data, setData] = useState(null);
+  let [requestParams, setRequestParams] = useState({});
+
+  const handleApiCall = (requestParams) => {
+    // mock output
+    const callApiData = {
+      count: 2,
+      results: [
+        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
+        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
+      ],
+    };
+    setData(callApiData);
+    setRequestParams(requestParams);
+  }
+
+  return (
+    <>
+      <Header />
+      <div>Request Method: {requestParams.method}</div>
+      <div>URL: {requestParams.url}</div>
+      <Form handleApiCall={handleApiCall} />
+      <Results data={data} />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
+/*
 class App extends React.Component {
 
   constructor(props) {
@@ -32,17 +65,7 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div>Request Method: {this.state.requestParams.method}</div>
-        <div>URL: {this.state.requestParams.url}</div>
-        <Form handleApiCall={this.callApi} />
-        <Results data={this.state.data} />
-        <Footer />
-      </React.Fragment>
-    );
+    
   }
 }
-
-export default App;
+*/
