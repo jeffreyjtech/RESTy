151d5b74c639d53bfd02a7c54ed26424cc4ee925
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-27 20:43:18 -0700

    Removed unused import

--- src/__tests__/submission-render.test.js
+++ src/__tests__/submission-render.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect';
 import axios from 'axios';
 import App from '../App'
