62bf98caaa128dd58879707c8654fb11a3aeb10f
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-27 19:07:20 -0700

    Removed commented out code from App

--- src/App.js
+++ src/App.js
@@ -40,32 +40,3 @@ function App() {
 }
 
 export default App;
-
-/*
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
-  }
-
-  callApi = (requestParams) => {
-    // mock output
-    const data = {
-      count: 2,
-      results: [
-        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
-        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
-      ],
-    };
-    this.setState({ data, requestParams });
-  }
-
-  render() {
-    
-  }
-}
-*/
